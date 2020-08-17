477faf1a56dddd5132388440c6bfa4f09072d40a
Lavish Sharma <lavish13sharma@gmail.com>
2020-08-17 21:03:36 +0530

    Update send_device_info.ino
    arduino update

--- Arduino/send_device_info/send_device_info.ino
+++ Arduino/send_device_info/send_device_info.ino
@@ -1,73 +1,68 @@
 #define output_pin 7
 #define device1_pin 8
-#define device2_pin 13
+#define device2_pin 9
 
-int state = 0;
+char inp[]={0,0};
+bool power=false;
 
 void send_device_info(){
-    char dev[] = "1:t,2:f"; // we can use this format : "pin_no : device_code"
+    String dev = "1:t,2:f"; // we can use this format : "pin_no : device_code"
     
 //    BTserial.print(dev);
-	Serial.print(dev); // one of these lines should work, unable to test
+  Serial.println(dev); // one of these lines should work, unable to test
 }
 
 
+/////////////////////////////////////////////////////////////////////
+//      case "11": // turn on device 1
+//      case "10": // turn off device 1
+//      case "21": // turn on device 2
+//      case "20": // turn off device 2 
+/////////////////////////////////////////////////////////////////////
 void perform_command(char inp[]){
-	int d = inp[0];
-	int c = inp[1];
-	
-	switch(d){
-		case '1': //code for device 1
-			if (c == '1') digitalWrite(device1_pin, HIGH);
-			else digitalWrite(device1_pin, LOW);
-			break;
-		case '2': //code for device 2
-			if (c == '1') digitalWrite(device2_pin, HIGH);
-			else digitalWrite(device2_pin, LOW);
-			break;
-	}
+  char d = inp[0];
+  char c = inp[1];
+  
+  switch(d){
+    case '1': //code for device 1
+      if (c == '1') digitalWrite(device1_pin, HIGH);
+      else digitalWrite(device1_pin, LOW);
+      break;
+    case '2': //code for device 2
+      if (c == '1') digitalWrite(device2_pin, HIGH);
+      else digitalWrite(device2_pin, LOW);
+      break;
+  }
 }
 
-
+void device() {
+  while(!power)
+  {
+    if(Serial.available())
+    {
+      char d=Serial.read();
+      if(d=='d')
+      {
+        send_device_info();
+        power=true;
+      }
+    }
+  }
+}
 void setup() {
     pinMode(output_pin, OUTPUT);
     // digitalWrite(output_pin, LOW);
-    pinMode(device1_pin, OUTPUT);
-    pinMode(device2_pin, OUTPUT);
     Serial.begin(9600);
-//    send_device_info();
 }
 
 void loop() {
-	
-	if (Serial.available() > 0){
-		String inp;
-		while(Serial.available() > 0){
-			inp += Serial.read();
-		}
-//		char inp[] = Serial.read();
-		
-		switch(inp){
-			case "d":
-				send_device_info();
-				break;
-			
-			case "11": // turn on device 1
-				perform_command("11");
-				break;
-			case "10": // turn off device 1
-				perform_command("10");
-				break;
-				
-			case "21": // turn on device 2
-				perform_command("21");
-				break;
-			case "20": // turn off device 2
-				perform_command("20");
-				break;
-			
-			default:
-				printf("Invalid State Entered!\n");
-		}
-	}
+  if(!power) device();
+  if(Serial.available() > 0 && power == true) {
+        for(int i=0;Serial.available() > 0;i++) {
+        inp[i] = Serial.read();
+      }
+      Serial.println(inp[0],inp[1]);
+    
+      perform_command(inp);
+    }
 }
