b09256e04f0d1221d8dd8669413353b29ca9e04a
Lavish Sharma <lavish13sharma@gmail.com>
2020-08-20 12:11:19 +0530

    android updates a

--- Android/app/src/main/java/com/example/homeautomation/MainActivity.java
+++ Android/app/src/main/java/com/example/homeautomation/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.homeautomation;
 
 import androidx.appcompat.app.AppCompatActivity;
-import  android.bluetooth.BluetoothAdapter;
-
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_ENABLE_BT = 1;
+
     BluetoothAdapter bluetooth;
+    TextView textview1;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +23,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         bluetooth = BluetoothAdapter.getDefaultAdapter();
+        if(bluetooth==null) {
+            showToast("Device incompatible");
+        }
+        textview1 = findViewById(R.id.textView1);
+        button = findViewById(R.id.button);
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!bluetooth.isEnabled()) {
+                    showToast("Turning On Bluetooth");
+                    Intent in = new Intent(bluetooth.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(in, REQUEST_ENABLE_BT);
+                }
+            }
+        });
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(bluetooth.isEnabled()) {
+            Intent intent = new Intent(MainActivity.this, PairedActivity.class);
+            startActivity(intent);
+        }
+    }
+    private void showToast(String msg) {
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
     }
 }
